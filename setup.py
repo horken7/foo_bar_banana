b1448ae99ebe39dc7c97de8689769d06f01b1d8d
horken7 <horken7@gmail.com>
2024-08-06 10:39:43 +0200

    clean up init file

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
 from setuptools import setup, find_packages
 
+
 def parse_requirements(filename):
     """Parse a requirements file into a list of strings."""
     with open(filename, 'r') as file:
         return [line.strip() for line in file if line.strip() and not line.startswith("#")]
+
 
 # Read requirements
 install_requires = parse_requirements('requirements.txt')
@@ -12,7 +14,7 @@ extra_requirements = {
 }
 
 setup(
-    name="foo-bar-banana",
+    name="foo_bar_banana",
     version="0.1",
     packages=find_packages(where='src'),
     package_dir={"": "src"},
